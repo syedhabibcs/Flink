16ecb97c3887c6383e8682a31370098678c265e7
habib <urrehmsh@myumanitoba.ca>
2018-12-01 18:54:37 -0600

    Refactor and random code

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, send_from_directory
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
 import os
+import random 
 
 import json
 import requests
@@ -16,7 +17,9 @@ CORS(app)
 
 class Server:
 
-    sendBox = {}
+    savedLinks = {}
+    savedFiles = {}
+    identifiers = []
     incrementer = 0
     
     @app.route('/',methods = ["GET","POST"])
@@ -25,23 +28,19 @@ class Server:
             
     @app.route("/send/",methods = ["GET","POST"])
     def getInfo():
-        hashedVal = 0
         if request.method == "POST":
-            if Server.incrementer >= 9999: 
-                Server.incrementer = 0
-                Server.sendBox = {}
-            sendMessage = request.json['sendBox']
-            Server.sendBox[str(Server.incrementer)] = sendMessage
-            hashedVal = str(Server.incrementer)
-            Server.incrementer = Server.incrementer+1
-        return hashedVal
+            ident = Server.get_identifier()
+            Server.savedLinks[ident] = request.json['sendBox']
+        return str(ident)
 
     @app.route("/receive/", methods=["GET", "POST"])
     def retrieveInfo():
         if request.method == "POST":
-            receivedCode = request.json['code']
-            if receivedCode in Server.sendBox:
-                receivedMessage = Server.sendBox[receivedCode]
+            receivedCode = int(request.json['code'])
+            if receivedCode in Server.savedLinks:
+                receivedMessage = Server.savedLinks[receivedCode]
+                Server.identifiers.remove(receivedCode)
+                del Server.savedLinks[receivedCode]
             else:
                 receivedMessage = 'Sorry no message found for the provided code.'
         return receivedMessage
@@ -49,14 +48,18 @@ class Server:
     @app.route('/upload/', methods = ['POST'])
     def upload_file():
         for k, file in request.files.items():
-            file.save(UPLOAD_DIRECTORY+secure_filename(str(Server.incrementer) + file.filename))
-            Server.sendBox[Server.incrementer] = str(Server.incrementer) + file.filename
-            Server.incrementer = Server.incrementer+1
-        return 'File uploaded successfully'
+            ident = Server.get_identifier()
+            file.save(UPLOAD_DIRECTORY+secure_filename(str(ident) + file.filename))
+            Server.savedFiles[ident] = str(ident) + file.filename
+        return str(ident)
 
     @app.route('/download/<code>')
     def download_file(code, methods = ['GET']):
-        return send_from_directory(app.config['UPLOAD_FOLDER'], Server.sendBox[int(code)])  
+        code = int(code)
+        file = send_from_directory(app.config['UPLOAD_FOLDER'], Server.savedFiles[code])
+        Server.identifiers.remove(code)
+        del Server.savedFiles[code]
+        return file
 
     def uploaded_files():
         files = []
@@ -65,6 +68,14 @@ class Server:
             if os.path.isfile(path):
                 files.append(filename)
         return files
+
+    def get_identifier():
+        while True:
+            i = random.randint(0, 9999)
+            if i not in Server.identifiers:
+                Server.identifiers.append(i)
+                return i
+        
 
 if __name__ == '__main__':
     server = Server()
