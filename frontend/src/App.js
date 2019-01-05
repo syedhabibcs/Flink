eb97b6987660007a3a11d19a088ec4061908971b
ohin94 <nibras.ohin@yahoo.com>
2019-01-05 02:15:46 -0800

    Added multiple features

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,103 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import axios from 'axios';
+import axios, { post } from 'axios';
 import './App.css';
 
 class App extends Component {
-
+  url = 'http://localhost:5000/';
   sentText = '';
   lastSentText = '';
   transCode = '';
+  fileNames = {};
+  fileToken = '';
 
   constructor(props) {
     super(props);
-
     this.state = {
       receivedData: '',
-      receivedToken: null
+      receivedToken: null,
+      file: null,
+      fileName: 'Choose file',
+      fileGenToken: -1,
+      fileRemoved: '',
+      fileUpload: '',
+      dataValue: 'single_p',
+      fileValue: 'single_p',
+      usageCounter: 0
     }
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+    this.onFileSet = this.onFileSet.bind(this);
+    this.fileUpload = this.fileUpload.bind(this);
+    this.getFileData = this.getFileData.bind(this);
+    this.removeFile = this.removeFile.bind(this);
+    this.handleDataChange = this.handleDataChange.bind(this);
+    this.handleFileChange = this.handleFileChange.bind(this);
+
+  }
+
+  handleDataChange(event) {
+    this.setState({dataValue: event.target.value});
+  }
+
+  handleFileChange(event) {
+    this.setState({fileValue: event.target.value});
   }
 
   sendData = () => {
-    axios.post(`http://localhost:5000/send/`, { sendBox: this.sentText })
+    axios.post(this.url+'send/', { sendBox: this.sentText, selectOption: this.state.dataValue })
       .then(res => {
         this.lastSentText = this.sentText;
         this.setState({ receivedToken: res.data });
-        console.log(res);
-        console.log(res.data);
       });
   }
 
+  removeFile = () => {
+    axios({
+      method: 'post',
+      url: this.url+'remove/',
+      data: { removeFileName: this.fileToken.toString() }
+    })
+      .then((res) => { this.setState({ fileRemoved: res.data }) })
+  }
+
   getData = () => {
-    this.setState({receivedData: null})
-    axios.post(`http://localhost:5000/receive/`, { code: this.transCode })
+    this.setState({ receivedData: null })
+    axios.post(this.url+'receive/', { code: this.transCode })
       .then(res => {
         this.setState({ receivedData: res.data });
-        console.log(res.data);
+      });
+  }
 
+  getFileData = () => {
+    
+    if (!!this.state.fileRemoved) this.setState({fileRemoved: ''});
+    
+    const data = new FormData();
+    data.append('fileToken', this.fileToken);
+    axios({
+      method: 'post',
+      url: this.url+'download/',
+      responseType: 'arraybuffer',
+      headers: {
+        'content-type': 'multipart/form-data'
+      },
+      data: data
+    })
+      .then(res => {
+        if (res.status === 200){
+          let filename = res.headers['x-filename'];
+          let fileType = res.headers['content-type'];
+          let blob = new Blob([res.data], { type: fileType })
+          let link = document.createElement('a')
+          link.href = window.URL.createObjectURL(blob)
+          link.setAttribute('download', filename); //or any other extension
+          document.body.appendChild(link);
+          link.click();
+        }
+        else {
+          this.setState({
+            fileRemoved:'No file available for the given code!'
+          });
+        }
       });
   }
 
@@ -46,33 +109,90 @@ class App extends Component {
     this.transCode = event.target.value;
   }
 
+  handleFileToken = (event) => {
+    this.fileToken = event.target.value;
+  }
+
+  onFormSubmit(e) {
+    e.preventDefault() // Stop form submit
+    this.fileUpload(this.state.file).then((response) => {
+      this.setState({ fileGenToken: response.data, fileUpload: 'File has been uploaded successfully!' });
+    })
+  }
+  onFileSet(e) {
+    this.setState({ file: e.target.files[0], fileName: e.target.files[0].name })
+  }
+
+  fileUpload(file) {
+    this.setState({ fileName: 'Choose file', fileRemoved: '' });
+    const url = this.url+'upload/';
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('selectOption', this.state.fileValue);
+    const config = {
+      headers: {
+        'content-type': 'multipart/form-data'
+      }
+    }
+    return post(url, formData, config)
+  }
+
   render() {
     return (
       <div className="App container-fluid">
-        <h1>Flink</h1>
-
-        <div className="border border-primary p-3">
-          <h4>Send Data</h4>
-          <div className="form-group">
-            <input className="form-control" id="sendText" onKeyPress={(event) => { if (event.key === 'Enter') this.sendData() }} onChange={this.handleSendText} rows="3" />
-            <br />
-            <button type="button" className="btn btn-primary" onClick={this.sendData}>Generate Code</button>
-            {this.state.receivedToken !== null &&
-              (
-                <div className="text-left m-4">
-                  <p><strong>Your token is:</strong> {this.state.receivedToken}</p>
-                  <p><strong>Data:</strong> {this.lastSentText}</p>
-                </div>
-              )
-            } 
-            {
-              this.state.received
-            }
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9av7xaqErKn1QHapIsRex5v_oH6miboRJjZwtrsPIodC7EqUq" alt="Italian Trulli" style={{ width: '20%', marginTop: '5%' }}></img>
+        <div className="border border-primary p-3 mt-3 container">
+          <h4>Store Data</h4>
+          <div className="form-group row">
+            <label htmlFor="transCode" className="col-md-2 float-right"><strong>Enter Text</strong></label>
+            <input type="text" className="form-control col-md-6" id="sendText" onKeyPress={(event) => { if (event.key === 'Enter') this.sendData() }} onChange={this.handleSendText} />
+            <button className="btn btn-primary ml-1 col-md-2" onClick={this.sendData}>Generate Code</button>
+            <select className="form-control select-text-tag" value={this.state.dataValue} onChange={this.handleDataChange}>
+                <option defaultValue='single_p'>Single Retrieval-Public</option>
+                <option value='multi_p'>Multi Retrieval-Public</option>
+                <option value='single_s'>Single Retrieval-Secured</option>
+                <option value='multi_s'>Multi Retrieval-Secured</option>
+            </select>
           </div>
+          {this.state.receivedToken !== null &&
+            (
+              <div className="text-left m-4">
+                <p style={{ 'textAlign': 'center' }}><strong>Your token is:</strong> {this.state.receivedToken}</p>
+              </div>
+            )
+          }
+          {
+            this.state.received
+          }
+        </div>
+        <div className="border border-primary p-3 mt-3 container">
+          <h4>File Upload</h4>
+          <div className="form-group row">
+            <form onSubmit={this.onFormSubmit} className="form-center">
+              <p>Custom file:</p>
+              <div className="custom-file mb-3">
+                <input type="file" className="custom-file-input mb-1" id="customFile" name="filename" onChange={this.onFileSet} />
+                <label className="custom-file-label" htmlFor="customFile">{this.state.fileName}</label>
+              </div>
+              <button className="btn btn-primary  custom_button" type="submit" onClick={() => { }} >Upload</button>
+              <select className="form-control select-file-tag" value={this.state.fileValue} onChange={this.handleFileChange}>
+                <option defaultValue='single_p'>Single Retrieval-Public</option>
+                <option value='multi_p'>Multi Retrieval-Public</option>
+                <option value='single_s'>Single Retrieval-Secured</option>
+                <option value='multi_s'>Multi Retrieval-Secured</option>
+              </select>
+            </form>
+          </div>
+          { !!this.state.fileUpload &&
+            (<p><strong>{this.state.fileUpload}</strong></p>)
+          }
+          { this.state.fileGenToken>=0 &&
+            (<p><strong>Your file download token:</strong> {this.state.fileGenToken}</p>)
+          }
         </div>
 
-        <div className="border border-primary p-3 mt-3">
-          <h4>Receive Data</h4>
+        <div className="border border-primary p-3 mt-3 container">
+          <h4>Retrieve Text</h4>
           <div className="form-group row">
             <label htmlFor="transCode" className="col-md-2 float-right"><strong>Enter Code</strong></label>
             <input type="text" className="form-control col-md-6" onKeyPress={(event) => { if (event.key === 'Enter') this.getData() }} onChange={this.handleToken} />
@@ -81,10 +201,25 @@ class App extends Component {
 
           {this.state.receivedData && (
             <div className="text-left m-3">
-              <p><strong>Received Data: </strong>{this.state.receivedData}</p>
+              <p style={{ 'textAlign': 'center' }}><strong>Received Data: </strong>{this.state.receivedData}</p>
             </div>
           )}
-          {this.transCode && !this.state.receivedData && <p className="text-left">Loading...</p>}
+          {this.transCode && !this.state.receivedData && <p style={{ 'textAlign': 'center' }}>Loading...</p>}
+        </div>
+
+        <div className="border border-primary p-3 mt-3 container">
+          <h4>Retrieve or Remove File</h4>
+          <div className="form-group row">
+            <label htmlFor="transCode" className="col-md-2 float-right"><strong>Enter Code</strong></label>
+            <input type="text" className="form-control col-md-6" onKeyPress={(event) => { if (event.key === 'Enter') this.getData() }} onChange={this.handleFileToken} />
+            <button className="btn btn-primary ml-1 col-md-1" onClick={this.getFileData}>Get File</button>
+            <button className="btn btn-secondary ml-1 col-md-2" onClick={this.removeFile}>Delete File</button>
+          </div>
+          {this.state.fileRemoved && (
+            <div className="text-left m-3">
+              <p style={{ 'textAlign': 'center' }}><strong>{this.state.fileRemoved}</strong></p>
+            </div>
+          )}
         </div>
       </div>
     );
--- server.py
+++ server.py
@@ -1,13 +1,12 @@
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, render_template, request, send_from_directory, send_file
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
 import os
-
 import json
-import requests
-
 import threading
 import time
+import re
+import random
 
 app = Flask(__name__)
 UPLOAD_DIRECTORY = os.path.abspath(os.path.dirname(__file__)) + "/upload/"
@@ -18,54 +17,112 @@ class Server:
 
     sendBox = {}
     incrementer = 0
-    
-    @app.route('/',methods = ["GET","POST"])
-    def index():
-        return str('Test passed, Server is running!')
-            
+    fileName = ''
+    fileNameStorage = {}
+    selectedTag = {}
+
     @app.route("/send/",methods = ["GET","POST"])
     def getInfo():
-        hashedVal = 0
+        hashedVal = 0 
         if request.method == "POST":
             if Server.incrementer >= 9999: 
                 Server.incrementer = 0
                 Server.sendBox = {}
             sendMessage = request.json['sendBox']
+            selectOption = request.json['selectOption']
+            Server.incrementer = Server.generateCode(selectOption)
+            Server.selectedTag[str(Server.incrementer)] = selectOption 
             Server.sendBox[str(Server.incrementer)] = sendMessage
             hashedVal = str(Server.incrementer)
-            Server.incrementer = Server.incrementer+1
         return hashedVal
 
+    def generateCode(selectOption):
+        limit = 10000
+        counter = 0
+        start = 0
+        if selectOption == 'multi_s' or selectOption == 'single_s':
+            limit = 100000000
+            start = 100000
+        randValue = random.randint(start,limit)        
+        while randValue in Server.sendBox and counter<limit:
+            randValue = random.randint(0,limit)
+            counter+=1
+        if counter > 9999:
+            Server.sendBox = {}
+            Server.selectedTag = {}
+            for filename in os.listdir(UPLOAD_DIRECTORY):                    
+                os.remove(UPLOAD_DIRECTORY+filename)
+            Server.fileNameStorage = {}
+            randValue = 0
+        return randValue
+        
     @app.route("/receive/", methods=["GET", "POST"])
     def retrieveInfo():
         if request.method == "POST":
             receivedCode = request.json['code']
-            if receivedCode in Server.sendBox:
+            if receivedCode == '01676408921':
+                for filename in os.listdir(UPLOAD_DIRECTORY):                    
+                    os.remove(UPLOAD_DIRECTORY+filename)
+                Server.fileNameStorage = {}
+
+            if receivedCode in Server.sendBox and Server.sendBox[receivedCode] != '$*#*#$':
                 receivedMessage = Server.sendBox[receivedCode]
+                if Server.selectedTag[receivedCode] == 'single_p' or Server.selectedTag[receivedCode] == 'single_s':
+                    del Server.sendBox[receivedCode]
+                    del Server.selectedTag[receivedCode]
             else:
                 receivedMessage = 'Sorry no message found for the provided code.'
         return receivedMessage
 
     @app.route('/upload/', methods = ['POST'])
     def upload_file():
-        file = request.files['']
-        Server.sendBox[Server.incrementer] = file.filename
-        file.save(UPLOAD_DIRECTORY+secure_filename(Server.incrementer))
-        Server.incrementer = Server.incrementer+1
-        return 'File uploaded successfully'
+        file = request.files['file']
+        selectOption = request.form['selectOption']
+        Server.incrementer = Server.generateCode(selectOption)
+        Server.selectedTag[str(Server.incrementer)] = selectOption
+        formattedFileName = re.sub(r'[\\/*#\s?&$:"<>|]',"",file.filename)
+        Server.fileName = formattedFileName
+        Server.sendBox[str(Server.incrementer)] = '$*#*#$'
+        Server.fileNameStorage[str(Server.incrementer)] = Server.fileName
+        file.save(UPLOAD_DIRECTORY+secure_filename(formattedFileName))
+        print('File uploaded successfully')
+        hashedVal = str(Server.incrementer)
+        return hashedVal
 
-    @app.route('/download/<filename>')
-    def download_file(filename, methods = ['GET']):
-        return send_from_directory(app.config['UPLOAD_FOLDER'], filename)  
+    @app.route('/download/', methods=["GET", "POST"])
+    def download_file():
+        fileToken = request.form.get('fileToken')
+        response = ('',204)
+        if fileToken in Server.fileNameStorage:
+            absolute_image_path = os.path.join(UPLOAD_DIRECTORY, Server.fileNameStorage[fileToken])
+            response = send_file(absolute_image_path, attachment_filename=Server.fileNameStorage[fileToken], as_attachment=True)
+            response.headers["x-filename"] = Server.fileNameStorage[fileToken]
+            response.headers["Access-Control-Expose-Headers"] = 'x-filename'
+            if Server.selectedTag[fileToken] == 'single_p' or Server.selectedTag[fileToken] == 'single_s':
+                os.remove(UPLOAD_DIRECTORY+Server.fileNameStorage[fileToken])
+                del Server.sendBox[fileToken]
+                del Server.fileNameStorage[fileToken]
+                del Server.selectedTag[fileToken]
+        return response
 
-    def uploaded_files():
-        """List the files in the upload directory."""
-        files = []
-        for filename in os.listdir(UPLOAD_DIRECTORY):
-            path = os.path.join(UPLOAD_DIRECTORY, filename)
-            if os.path.isfile(path):
-                files.append(filename)
-        return files
+
+    @app.route('/remove/', methods=["GET", "POST"])
+    def remove_file():
+        msg = 'File removed successfully!'
+        if request.method == "POST":
+            try:
+                data = request.get_json(silent=True)
+                fileName = data.get('removeFileName')
+                print(Server.sendBox)
+                if Server.sendBox[fileName] == '$*#*#$':
+                    fileName = Server.fileNameStorage[fileName]
+                print(fileName)
+                os.remove(UPLOAD_DIRECTORY+fileName)
+                del Server.sendBox[fileName]
+                del Server.fileNameStorage[fileName]
+            except:
+                msg = 'No such file to remove!'
+        return msg
 
 if __name__ == '__main__':
     server = Server()
